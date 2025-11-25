7660f9df61bb36329be9b223dd356569561accdb
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2025-11-25 23:17:26 +0700

    Sync dotfiles

--- .config/fish/conf.d/functions.fish
+++ .config/fish/conf.d/functions.fish
@@ -38,7 +38,7 @@ function y
 	set tmp (mktemp -t "yazi-cwd.XXXXXX")
 	yazi $argv --cwd-file="$tmp"
 	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
+		z -- "$cwd"
 	end
 	rm -f -- "$tmp"
 end
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,8 +5,18 @@ if status is-interactive
     fzf --fish | source
 
     # VS Code Shell Integration
-    if type -q cygpath; and test "$TERM_PROGRAM" = "vscode"
+    if set -q MSYSTEM; and type -q cygpath; and test "$TERM_PROGRAM" = "vscode"
         source (cygpath -u (code --locate-shell-integration-path fish))
     end
 
+    if set -q MSYSTEM; and type -q cygpath
+        function __fish_update_cwd_osc --on-variable PWD --description 'Notify terminals when $PWD changes'
+            set -l host $hostname
+            if [ "$TERM" = dumb ]
+                return
+            end
+            set -l win_path (cygpath -m -a "$PWD")
+            printf \e\]7\;file://%s%s\a $host (string escape --style=url -- $win_path)
+        end
+    end
 end
